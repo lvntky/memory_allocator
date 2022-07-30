bd359dba67c0ec15be80678974715c5588f56771
lvntky <klevent1903@gmail.com>
2022-07-30 20:06:13 +0300

    refactor metadata structue

--- main.c
+++ main.c
@@ -3,8 +3,8 @@
 #include <stdbool.h>
 
 #define MEMORY_CAPACITY 640000
-#define MEMORY_ALLOC_CAPACITY 1024
-#define MEMORY_FREED_CAPACITY 1024
+#define CHUNK_LIST_CAPACITY 1024
+
 
 // Cust kind of meta-data for 
 // Allocated memory on the heap...
@@ -13,11 +13,52 @@ typedef struct {
 	size_t size;
 } Memory_Chunk;
 
+typedef struct
+{
+	size_t count;
+	Memory_Chunk chunks[CHUNK_LIST_CAPACITY];
+} Chunk_List;
+
+int chunk_list_find(Chunk_List *chunk, void *ptr)
+{
+	assert(false && "not implemented yet");
+	return -1;
+}
+
+void chunk_list_remove(Chunk_List *list, size_t index) {
+	assert(false && "not implemented yet");
+}
+
+void chunk_list_insert(Chunk_List *list, void* start, size_t size) {
+	assert(list->count < CHUNK_LIST_CAPACITY);
+	list->chunks[list->count].start = start;
+	list->chunks[list->count].size = size;
+	for(size_t i = list->count; i > 0 && list->chunks[i].start < list->chunks[i - 1].start; --i) {
+		const Memory_Chunk t = list->chunks[i];
+		list->chunks[i] = list->chunks[i - 1];
+		list->chunks[i - 1 ] = list->chunks[i];
+	}	
+	list->count += 1;
+}
+
+// dumping function to iterate and see whats in the heap
+// with healp of meta-data struct to log
+void chunk_list_dump(const Chunk_List *chunk)
+{
+	printf("Chunks ( %zu ):\n", chunk->count);
+	for(int i = 0; i < chunk->count; ++i) {
+		printf(" starts: %p, size: %zu\n", 
+				chunk->chunks[i].start,
+				chunk->chunks[i].size);
+	}
+}
+
+
 char memory[MEMORY_CAPACITY];
 size_t memory_size = 0;
 
-Memory_Chunk memory_alloced[MEMORY_ALLOC_CAPACITY] = {0};
-size_t memory_alloced_size = 0;
+Chunk_List allocaded_chunks = {0};
+Chunk_List freed_chunks = {0};
 
 void *memory_allocate(size_t size)
 {
@@ -25,12 +66,7 @@ void *memory_allocate(size_t size)
 		assert(size + memory_size <= MEMORY_CAPACITY);
 		void *result = memory + memory_size;
 		memory_size += size;
-		Memory_Chunk chunk = {
-			.start = result,
-			.size = size,
-		};
-		assert(memory_alloced_size < MEMORY_ALLOC_CAPACITY);
-		memory_alloced[memory_alloced_size++] = chunk;
+		chunk_list_insert(&allocaded_chunks, result, size);
 		return result;
 	}
 	// return null in case of user pass 0 size to allocate
@@ -39,28 +75,11 @@ void *memory_allocate(size_t size)
 	}
 }
 
-// dumb function to iterate and see whats in the heap
-// with healp of meta-data struct to log
-void mem_dump_alloced_chunks(void)
-{
-	printf("Allocated Chunks in the heap ( %zu ):\n", memory_alloced_size);
-	for(int i = 0; i < memory_alloced_size; ++i) {
-		printf(" starts: %p, size: %zu\n", 
-				memory_alloced[i].start,
-				memory_alloced[i].size);
-	}
-}
 
-Memory_Chunk memory_freed[MEMORY_FREED_CAPACITY] = {0};
-size_t memory_freed_size = 0;
 // O(Alloceted)
 void memory_free(void *ptr)
 {
-	for(size_t i = 0; i < memory_alloced_size; ++i) {
-		if(memory_alloced->start == ptr) {
-			// CHUNG BASLANGIC NOKTASI BULUNDU
-		}
-	}
+	assert(false && "free implemet edilecek");	
 }
 
 int main(int argc, char** argv)
@@ -68,9 +87,10 @@ int main(int argc, char** argv)
 	for(int i = 0; i < 100; ++i){
 		void *p = memory_allocate(i);
 		if(i % 2 == 0){
-			memory_free(p);
+			//memory_free(p);
 		}
 	}
-	mem_dump_alloced_chunks();
+	chunk_list_dump(&allocaded_chunks);
+	
 	return 0;
 }
